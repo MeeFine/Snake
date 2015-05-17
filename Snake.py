1176a82f643054f42b71d2eceb90983edf7bc132
weiyou16 <weiyou16@ougl-66-c.eplt.washington.edu>
2015-05-17 16:30:19 -0700

    curses UI initialization added

--- Snake.py
+++ Snake.py
@@ -1,4 +1,21 @@
 import curses
+# Initial user interface: Curses
+#initializes curses to use it
+stdscr = curses.initscr()
+# avoid the need to echo to the screen
+curses.noecho()
+# reacts to the keys instantly without requiring the enter key to be pressed
+curses.cbreak()
+#enables the uses of normal and special keys
+stdscr.keypad(True)
+
+height = 10
+width = 15
+begin_x = 5
+begin_y = 5
+#creates a new window of a given size, returning the new window object.
+win = curses.newwin(height, width, begin_x, begin_y)
+
 
 #function to run the snake with BFSS
 def runBFS():
