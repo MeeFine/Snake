11aff295db3e12010141bfd3e7645a7697c315b6
weiyou16 <weiyou16@ougl-08-b.eplt.washington.edu>
2015-05-29 12:07:37 -0700

    Small changes

--- Snake.py
+++ Snake.py
@@ -1,22 +1,39 @@
 import curses
 # Initial user interface: Curses
 #initializes curses to use it
-stdscr = curses.initscr()
+curses.initscr()
 # avoid the need to echo to the screen
 curses.noecho()
 # reacts to the keys instantly without requiring the enter key to be pressed
 curses.cbreak()
-#enables the uses of normal and special keys
-stdscr.keypad(True)
+curses.curs_set(0)
 
-height = 10
-width = 15
-begin_x = 5
-begin_y = 5
+height = 20
+width = 50
+begin_x = 0
+begin_y = 0
 #creates a new window of a given size, returning the new window object.
 win = curses.newwin(height, width, begin_x, begin_y)
 
+win.keypad(True)
+win.border(0)
+win.nodelay(1)
+# the location of the apple
+win.addch(0, 0, '@')
+event = win.getch()
+#enables the uses of normal and special keys
+win.keypad(True)
+
+APPLE = 0
+POINT = 0
+
+while (1):
+    win.border(0)
+    win.addch(0, 3, 'P')
+    event = win.getch()
 
 #function to run the snake with BFSS
 def runBFS():
     return ""
+
+curses.endwin()
