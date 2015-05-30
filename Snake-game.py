3694524c403c7cedfec9834e9d32a52932acd292
dwy189 <daiweiyou16@hotmail.com>
2015-05-29 19:59:44 -0700

    Snake-game and Test

--- Snake-game.py
+++ Snake-game.py
@@ -2,6 +2,7 @@ import curses
 import time
 import random
 import sys
+from copy import copy
 
 screen = curses.initscr()
 dims = screen.getmaxyx()
@@ -9,14 +10,18 @@ def game():
     screen.nodelay(1)
     head = [1, 1]
     score = 0
-    body = [head[:]]*5
+    #default body size = 3
+    body = copy(head)
+    body = body * 3
 
     screen.border()
-
+    previous = copy(body[-1])
     direction = 0 # 0: right, 1: down, 2: left, 3: up
     gameOver = False
 
     while not gameOver:
+        if (previous not in body):
+            screen.addch(previous[0], previous[1], ' ')
         screen.addch(head[0], head[1], '*')
         if (direction == 0):
             head[1] += 1
@@ -27,8 +32,9 @@ def game():
         elif (direction == 3):
             head[0] -= 1
 
-        for z in range(0, len(body)-1, 1):
-            body[z] = body[z-1]
+        previousBody = copy(body[-1])
+        for i in range(len(body)-1, 0, -1):
+            body[i] = copy(body[i-1])
         # if it touches something other than the space, then game over
         if screen.inch(head[0], head[1]) != ord(' '):
             gameOver = True
